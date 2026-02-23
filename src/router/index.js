bc9f5537abcd284caf71613aef7581aea4cc03a9
Maria Timofejev <maria.timofejev.001@mail.ee>
2026-02-23 15:24:49 +0200

    refactor(router): implement lazy loaded views and named routes
    - moved pages to views directory
    - enabled dynamic imports for code splitting
    - added route props for locality detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LocalityList from "./LocalitiesList.vue";
-import LocalityDetail from "./LocalityDetail.vue";
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: LocalityList },
-    { path: "/locality/:id", component: LocalityDetail }
+    {
+      path: "/",
+      name: "localities",
+      component: () => import("@/views/LocalitiesList.vue")
+    },
+    {
+      path: "/locality/:id",
+      name: "locality-detail",
+      component: () => import("@/views/LocalityDetail.vue"),
+      props: true
+    }
   ]
 });
+
+export default router;
