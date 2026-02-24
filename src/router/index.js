8e297d9ccd99ac2a9ef8f3e2f667005239f0dd18
Maria Timofejev <maria.timofejev.001@mail.ee>
2026-02-24 15:00:38 +0200

    Add documented Vue Router configuration with lazy-loaded routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Vue Router instance using HTML5 history (clean URLs without #)
 const router = createRouter({
   history: createWebHistory(),
+
+// Application routes
   routes: [
     {
       path: "/",
       name: "localities",
+
+// Lazy-loaded view to reduce initial bundle size
       component: () => import("@/views/LocalitiesList.vue")
     },
     {
       path: "/locality/:id",
       name: "locality-detail",
+
+// Dynamic import enables code splitting per page
       component: () => import("@/views/LocalityDetail.vue"),
+
+// Pass route params as component props (better testability & decoupling)
       props: true
     }
   ]
